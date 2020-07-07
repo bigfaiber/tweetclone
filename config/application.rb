fb5062709d4c96781ab93f2b267728cc5e174490
bigfaiber <greatfaiber@gmail.com>
2020-07-07 10:32:35 -0500

    devise and rspec-suite

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Tweetclone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -15,5 +16,13 @@ module Tweetclone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_bot_rails, dir: 'spec/factories'
+
+      g.view_specs false
+      g.helper_specs false
+    end
+
   end
 end
