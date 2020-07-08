e183973bd487a1fac07bc4eeb1d8e910baf2770b
bigfaiber <greatfaiber@gmail.com>
2020-07-07 22:55:50 -0500

    remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     get 'follows/following'
   end
   devise_for :users
-  resource :tweets
+  resource :tweets, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
